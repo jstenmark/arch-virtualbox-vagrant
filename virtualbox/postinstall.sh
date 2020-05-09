b622ae5b0973229bebd9b067d1c9a4ad51aa250b
jstenmark <johannes@stenmark.in>
2020-05-09 02:19:52 +0200

    add vbox X support

--- virtualbox/postinstall.sh
+++ virtualbox/postinstall.sh
@@ -2,7 +2,6 @@
 set -e
 set -x
 
-# setting hostname, locales, etc
 hostnamectl set-hostname arch
 localectl set-keymap sv-latin1
 timedatectl set-ntp true
--- virtualbox/virtualbox.sh
+++ virtualbox/virtualbox.sh
@@ -1,16 +1,14 @@
 #!/bin/bash
-
 set -e
 set -x
 
-sudo pacman -S --noconfirm virtualbox-guest-utils-nox
+# Guest services
+sudo pacman -S --noconfirm virtualbox-guest-utils xf86-video-vmware
 sudo systemctl enable vboxservice
 
-# VirtualBox Guest Additions
-#sudo pacman -S --noconfirm linux-headers virtualbox-guest-utils virtualbox-guest-dkms nfs-utils
-#sudo echo -e 'vboxguest\nvboxsf\nvboxvideo' > /etc/modules-load.d/virtualbox.conf
-#guest_version=\$(pacman -Q virtualbox-guest-dkms | awk '{ print \$2 }' | cut -d'-' -f1)
-#kernel_version="\$(pacman -Q linux | awk '{ print \$2 }')-ARCH"
-#sudo dkms install "vboxguest/\${guest_version}" -k "\${kernel_version}/x86_64"
-#sudo systemctl enable dkms
-#sudo systemctl enable rpcbind
+VBoxClient --clipboard
+VBoxClient --draganddrop
+VBoxClient --seamless
+VBoxClient --display
+VBoxClient --checkhostversion
+VBoxClient --vmsvga-x11
